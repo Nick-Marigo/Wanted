dfece697fe7cb48e6b4875d74fb512d1fc9c9569
Nick Marigo <118312264+Nick-Marigo@users.noreply.github.com>
2026-06-15 00:00:10 -0700

    Added two debug keys. R to restart scene. G to turn on debug boxes

--- src/scenes/play.js
+++ src/scenes/play.js
@@ -15,7 +15,26 @@ class Play extends Phaser.Scene {
 
         const randomSpawn = this.createRandomSpawnPoint();
 
+        this.UnknwonArray = [];
+
         this.unknwon = new Unknwon(this, randomSpawn.x, randomSpawn.y, 'slug');
+
+
+        // DEBUG: R key to restart scene
+        this.input.keyboard.on('keydown-R', () => {
+            this.scene.restart();
+        });
+
+        // DEBUG: G key to draw debug boxes on/off
+        this.physics.world.createDebugGraphic();
+        this.physics.world.drawDebug = false;
+        this.input.keyboard.on('keydown-G', () => {
+            this.physics.world.drawDebug = !this.physics.world.drawDebug;
+
+            if (!this.physics.world.drawDebug) {
+                this.physics.world.debugGraphic.clear();
+            }
+        });
 
     }
 
